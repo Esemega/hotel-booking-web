692ed1ac05c704f8b9981c7ce447676348e22709
Esemega <sogaral89@gmail.com>
2020-12-15 19:24:08 +0100

    ParticularClient class done (ca (Case 1)

--- app.js
+++ app.js
@@ -24,6 +24,11 @@ const bookings = [
     pax: 2,
     nights: 1,
   },
+  {
+    roomType: "standard",
+    pax: 2,
+    nights: 4,
+  },
 ];
 
 /** *** CASE 1 - PARTICULAR CLIENT ***
@@ -35,9 +40,72 @@ const bookings = [
 *    IVA + 21% (total)
 *
 * - Class get the bookings list and return subtotal and total
- */
+*/
 
- /** *** CASE 2 - TOUR OPERATOR ***
+class ParticularClient {
+    constructor() {
+        this._bookings = [];
+        this._subtotal = 0;
+        this._total = 0;
+    }
+
+    getRoomPrice(roomType) {
+        switch (roomType) {
+          case "standard":
+            return 100;
+          case "suite":
+            return 150;
+        }
+        return 1;
+    }
+
+    getExtra(pax) {
+        return pax > 1 ? (pax - 1 )*40 : 0;
+    }
+
+    getIVA() {
+        return 1.21;
+    }
+
+    calculateSubtotal() {
+        this._subtotal = bookings.reduce(
+            (acc, { roomType, pax, nights }) => acc + (nights * (this.getRoomPrice(roomType) + this.getExtra(pax))),
+            0
+        );
+    }
+    
+    
+    calculateTotal() {
+        this._total = bookings.reduce(
+            (acc, { roomType, pax, nights }) => acc + (nights * (this.getRoomPrice(roomType) + this.getExtra(pax))) * this.getIVA(),
+            0
+        );
+    }
+
+    get total() {
+    return this._total;
+    }
+    
+    get subtotal() {
+    return this._subtotal;
+    }
+
+    set bookings(outerBookings) {
+    // if bookings is changed all bellow will happend
+    this._bookings = outerBookings;
+    this.calculateSubtotal(); // subtotal is updated
+    this.calculateTotal(); // total is updated
+    }
+}
+
+const particularClient = new ParticularClient();
+
+particularClient.bookings = bookings;
+
+console.log("Subtotal", particularClient.subtotal);
+console.log("Total", particularClient.total);
+
+/** *** CASE 2 - TOUR OPERATOR ***
 *   Room / night (IVA No Incluided):
 *        100€ all rooms
 *   Cargos adicionales:
@@ -47,4 +115,4 @@ const bookings = [
 *    IVA + 21% (total)
 *
 * - Class get the bookings list and return subtotal and total
- */
+*/
